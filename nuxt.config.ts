46f56b4c36ea3c53812256c356cbcf0a8be47565
Ilyas Turki <turki.ilyass@gmail.com>
2026-06-01 17:32:31 +0200

    chore: replace @nuxt/fonts module with @nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
     devtools: { enabled: true },
-    modules: ['@vueuse/nuxt', '@nuxt/fonts'],
+    modules: ['@vueuse/nuxt', '@nuxt/ui'],
     css: ['./app/assets/css/main.css'],
     vite: {
         plugins: [tailwindcss()],
