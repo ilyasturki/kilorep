7dbb3595e51b63e64c5fad78d9bac98bb2c9fc19
Ilyas Turki <turki.ilyass@gmail.com>
2026-06-09 13:48:44 +0200

    chore(nuxt): remove devtools config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
-    devtools: { enabled: true },
     modules: ['@vueuse/nuxt', 'reka-ui/nuxt'],
     css: ['./app/assets/css/main.css'],
     app: {
