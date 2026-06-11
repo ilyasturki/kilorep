749e251ba28b50f68d65399a6cf50c974b80e71f
Ilyas Turki <turki.ilyass@gmail.com>
2026-06-12 00:26:22 +0200

    chore(nuxt): disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     compatibilityDate: '2025-07-15',
+    devtools: { enabled: false },
     modules: [
         '@vueuse/nuxt',
         'reka-ui/nuxt',
